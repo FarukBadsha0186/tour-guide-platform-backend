6d9c4536c6bc2ffec4f22386ff40cd301fc0985b
FarukBadsha0186 <farukbadsha02@gmail.com>
2026-09-18 03:41:44 +0600

    fix: add seed property and main function for production seed

--- src/app/utils/seed.ts
+++ src/app/utils/seed.ts
@@ -1,20 +1,249 @@
+// import { Role } from "../../generated/prisma/enums"
+// import config from "../config";
+// import { prisma } from "../lib/prisma"
+// import bcrypt from "bcryptjs";
+
+// export const  seedAdmin = async() =>{
+//     try {
+//         const isSuperAdminExist = await prisma.user.findFirst({
+//             where :{
+//                 role: Role.ADMIN
+//             }
+
+//         });
+//         if(isSuperAdminExist){
+//             console.log(" Admin Already Exists!")
+//             return;
+
+//         }
+//         const name =config.tester_admin_name
+//         const email =config.tester_admin_email
+//         const password =config.tester_admin_password
+
+//         if(!name || !email || !password ){
+//             throw new Error(" Admin Name ,Email ,Password Missing in env file");
+            
+//         }
+
+//         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
+        
+//         const superAdmin =await prisma.user.create({
+//             data:{
+//                 name,
+//                 email,
+//                 password :hashedPassword,
+//                 role:Role.ADMIN,
+//                 needPasswordChange:false,
+//                 emailVerified:true
+//             }
+//         })
+
+//         console.log(" Admin Created ", superAdmin);
+
+
+
+//     } catch (error) {
+//         console.log("Error seeding Super Admin :", error);
+
+//         await prisma.user.delete({
+//             where:{
+//                 email: config.tester_admin_email,
+
+//             }
+//         })
+
+        
+        
+//     }
+// }
+
+
+// export const  seedTesterAdmin = async() =>{
+//     try {
+//         const isSuperAdminExist = await prisma.user.findUnique({
+//             where :{
+//                 email:config.tester_admin_email
+//             }
+
+//         });
+//         if(isSuperAdminExist){
+//             console.log("Tester Admin Already Exists!")
+//             return;
+
+//         }
+//         const name =config.tester_admin_name
+//         const email =config.tester_admin_email
+//         const password =config.tester_admin_password
+
+//         if(!name || !email || !password ){
+//             throw new Error("Tester Admin Name ,EMail ,Password Missing in env file");
+            
+//         }
+
+//         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
+        
+//         const testerAdmin =await prisma.user.create({
+//             data:{
+//                 name,
+//                 email,
+//                 password :hashedPassword,
+//                 role:Role.ADMIN,
+//                 needPasswordChange:false,
+//                 emailVerified:true
+//             }
+//         })
+
+//         console.log("Tester Admin Created ", testerAdmin);
+
+
+
+//     } catch (error) {
+//         console.log("Error seeding Super Admin :", error);
+
+//         await prisma.user.delete({
+//             where:{
+//                 email: config.tester_admin_email,
+
+//             }
+//         })
+
+        
+        
+//     }
+// }
+
+// export const  seedTesterGuide = async() =>{
+//     try {
+//         const isTEsterDoctorExist = await prisma.user.findUnique({
+//             where :{
+//                 email:config.tester_guide_email
+//             }
+
+//         });
+//         if(isTEsterDoctorExist){
+//             console.log("GUIDE  Already Exists!")
+//             return;
+
+//         }
+//         const name =config.tester_guide_name;
+//         const email =config.tester_guide_email
+//         const password =config.tester_guide_password
+
+//         if(!name || !email || !password ){
+//             throw new Error("Guide Name ,Email ,Password Missing in env file");
+            
+//         }
+
+//         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
+        
+//         const doctor =await prisma.user.create({
+//             data:{
+//                 name,
+//                 email,
+//                 password :hashedPassword,
+//                 role:Role.GUIDE,
+//                 needPasswordChange:false,
+//                 emailVerified:true
+//             }
+//         })
+
+//         console.log("Guide  Created ", doctor);
+
+
+
+//     } catch (error) {
+//         console.log("Error seeding Doctor  :", error);
+
+//         await prisma.user.delete({
+//             where:{
+//                 email: config.tester_guide_email,
+
+//             }
+//         })
+
+        
+        
+//     }
+// }
+
+
+// export const  seedTesterTourist = async() =>{
+//     try {
+//         const isTEsterTouristExist = await prisma.user.findUnique({
+//             where :{
+//                 email:config.tester_tourist_email
+//             }
+
+//         });
+//         if(isTEsterTouristExist){
+//             console.log("Tourist  Already Exists!")
+//             return;
+
+//         }
+//         const name =config.tester_tourist_name;
+//         const email =config.tester_tourist_email
+//         const password =config.tester_tourist_password
+
+//         if(!name || !email || !password ){
+//             throw new Error("Tourist Name ,Email ,Password Missing in env file");
+            
+//         }
+
+//         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
+        
+//         const tourist =await prisma.user.create({
+//             data:{
+//                 name,
+//                 email,
+//                 password :hashedPassword,
+//                 role:Role.TOURIST,
+//                 needPasswordChange:false,
+//                 emailVerified:true
+//             }
+//         })
+
+//         console.log("Guide  Created ", tourist);
+
+
+
+//     } catch (error) {
+//         console.log("Error seeding Tourist  :", error);
+
+//         await prisma.user.delete({
+//             where:{
+//                 email: config.tester_tourist_email,
+
+//             }
+//         })
+
+        
+        
+//     }
+
+    
+// }
+
+// src/app/utils/seed.ts
+
 import { Role } from "../../generated/prisma/enums"
 import config from "../config";
 import { prisma } from "../lib/prisma"
 import bcrypt from "bcryptjs";
 
-export const  seedAdmin = async() =>{
+// =============================================
+// ========== SEED FUNCTIONS ==========
+// =============================================
+
+export const seedAdmin = async() =>{
     try {
         const isSuperAdminExist = await prisma.user.findFirst({
             where :{
                 role: Role.ADMIN
             }
-
         });
         if(isSuperAdminExist){
             console.log(" Admin Already Exists!")
             return;
-
         }
         const name =config.tester_admin_name
         const email =config.tester_admin_email
@@ -22,7 +251,6 @@ export const  seedAdmin = async() =>{
 
         if(!name || !email || !password ){
             throw new Error(" Admin Name ,Email ,Password Missing in env file");
-            
         }
 
         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
@@ -39,37 +267,26 @@ export const  seedAdmin = async() =>{
         })
 
         console.log(" Admin Created ", superAdmin);
-
-
-
     } catch (error) {
         console.log("Error seeding Super Admin :", error);
-
         await prisma.user.delete({
             where:{
                 email: config.tester_admin_email,
-
             }
         })
-
-        
-        
     }
 }
 
-
-export const  seedTesterAdmin = async() =>{
+export const seedTesterAdmin = async() =>{
     try {
         const isSuperAdminExist = await prisma.user.findUnique({
             where :{
                 email:config.tester_admin_email
             }
-
         });
         if(isSuperAdminExist){
             console.log("Tester Admin Already Exists!")
             return;
-
         }
         const name =config.tester_admin_name
         const email =config.tester_admin_email
@@ -77,7 +294,6 @@ export const  seedTesterAdmin = async() =>{
 
         if(!name || !email || !password ){
             throw new Error("Tester Admin Name ,EMail ,Password Missing in env file");
-            
         }
 
         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
@@ -93,37 +309,22 @@ export const  seedTesterAdmin = async() =>{
             }
         })
 
-        console.log("Tester Admin Created ", testerAdmin);
-
-
-
+        console.log("✅ Tester Admin Created ", testerAdmin.email);
     } catch (error) {
-        console.log("Error seeding Super Admin :", error);
-
-        await prisma.user.delete({
-            where:{
-                email: config.tester_admin_email,
-
-            }
-        })
-
-        
-        
+        console.log("Error seeding Tester Admin :", error);
     }
 }
 
-export const  seedTesterGuide = async() =>{
+export const seedTesterGuide = async() =>{
     try {
         const isTEsterDoctorExist = await prisma.user.findUnique({
             where :{
                 email:config.tester_guide_email
             }
-
         });
         if(isTEsterDoctorExist){
             console.log("GUIDE  Already Exists!")
             return;
-
         }
         const name =config.tester_guide_name;
         const email =config.tester_guide_email
@@ -131,7 +332,6 @@ export const  seedTesterGuide = async() =>{
 
         if(!name || !email || !password ){
             throw new Error("Guide Name ,Email ,Password Missing in env file");
-            
         }
 
         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
@@ -147,38 +347,22 @@ export const  seedTesterGuide = async() =>{
             }
         })
 
-        console.log("Guide  Created ", doctor);
-
-
-
+        console.log("✅ Guide Created ", doctor.email);
     } catch (error) {
-        console.log("Error seeding Doctor  :", error);
-
-        await prisma.user.delete({
-            where:{
-                email: config.tester_guide_email,
-
-            }
-        })
-
-        
-        
+        console.log("Error seeding Guide :", error);
     }
 }
 
-
-export const  seedTesterTourist = async() =>{
+export const seedTesterTourist = async() =>{
     try {
         const isTEsterTouristExist = await prisma.user.findUnique({
             where :{
                 email:config.tester_tourist_email
             }
-
         });
         if(isTEsterTouristExist){
             console.log("Tourist  Already Exists!")
             return;
-
         }
         const name =config.tester_tourist_name;
         const email =config.tester_tourist_email
@@ -186,7 +370,6 @@ export const  seedTesterTourist = async() =>{
 
         if(!name || !email || !password ){
             throw new Error("Tourist Name ,Email ,Password Missing in env file");
-            
         }
 
         const hashedPassword = await bcrypt.hash(password ,Number(config.bcrypt_salt_rounds))
@@ -202,21 +385,31 @@ export const  seedTesterTourist = async() =>{
             }
         })
 
-        console.log("Guide  Created ", tourist);
-
-
-
+        console.log("✅ Tourist Created ", tourist.email);
     } catch (error) {
-        console.log("Error seeding Tourist  :", error);
-
-        await prisma.user.delete({
-            where:{
-                email: config.tester_tourist_email,
-
-            }
-        })
-
-        
-        
+        console.log("Error seeding Tourist :", error);
     }
 }
+
+// =============================================
+// ========== MAIN FUNCTION (NEW!) ==========
+// =============================================
+async function main() {
+  try {
+    console.log("🌱 Starting seed...\n");
+
+    await seedTesterAdmin();
+    await seedTesterGuide();
+    await seedTesterTourist();
+
+    console.log("\n🎉 Seeding complete!");
+  } catch (error) {
+    console.error("❌ Seed error:", error);
+    process.exit(1);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+// Execute
+main();
